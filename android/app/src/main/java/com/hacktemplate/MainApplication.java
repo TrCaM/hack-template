270f3a9aa5b629b9f3041f990c02e26baa99a669
TrCaM <cmthscb@gmail.com>
2020-01-10 01:40:04 -0500

    Finish importing Google-sign-in

--- android/app/src/main/java/com/hacktemplate/MainApplication.java
+++ android/app/src/main/java/com/hacktemplate/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import java.util.Arrays;
 import java.util.List;
 
