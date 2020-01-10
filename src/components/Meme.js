ce0491a25561ccf59ffcca4462ab2453b247abd6
HsWin <thangminhcao@gmail.com>
2020-01-10 16:44:21 -0500

    My list item component

---
+++ src/components/Meme.js
@@ -0,0 +1,17 @@
+const list = [
+    {
+        url: "https://www.syfy.com/sites/syfy/files/styles/1200x680/public/2018/04/screen_shot_2018-04-09_at_2.00.58_pm.png", 
+    },
+
+    {
+        url: "https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg",
+    },
+    {
+        url: "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg", 
+    },
+
+    {
+        url: "https://www.w3schools.com/css/img_lights.jpg",
+    },
+]
+export default list;
