0c52e1d28c747dc1c08af7b71f13f7cfe92b071b
quanghienle <quanghien.a1@gmail.com>
2020-01-10 16:04:44 -0500

    get memes

--- src/screens/HomeScreen/TrendingScreen.js
+++ src/screens/HomeScreen/TrendingScreen.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import {Text, View, Button} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, Button, Image} from 'react-native';
+import getMemeUrl from '../../services/getMemeUrl';
 
 const TrendingScreen = props => {
   const goToLogin = () => {
     props.navigation.navigate('Login');
   };
 
+  const [imageUrl, setImageUrl] = useState('');
+
+  const getMeme = async () => {
+    const url = await getMemeUrl();
+    setImageUrl(url);
+    console.log(url);
+  };
+
   return (
-    <View style={{padding: 100}}>
+    <View style={{padding: 50}}>
       <Text>Trending</Text>
+      {/* <Text>{imageUrl}</Text> */}
       <Button title="go to Login" onPress={goToLogin} />
+      <Button title="get image" onPress={getMeme} />
+      <Image
+        style={{width: 350, height: 350}}
+        source={{
+          uri: imageUrl,
+        }}
+      />
     </View>
   );
 };
---
+++ src/services/getMemeUrl.js
@@ -0,0 +1,26 @@
+const MEME_TYPES = ['dank', 'moderate', 'light', 'furry'];
+const memeType = MEME_TYPES[Math.floor(Math.random() * MEME_TYPES.length)];
+
+const getImagePromise = async function() {
+  const axios = require('axios').default;
+  const url = 'https://meme-api.glitch.me/' + memeType;
+  try {
+    const response = await axios.get(url);
+    return response.data.meme;
+  } catch (error) {
+    return error;
+  }
+};
+
+const memeUrl = async () => {
+  const acceptedType = ['png', 'jpg', 'gif'];
+  let fileType = link => link.split('.')[2];
+  let url = await getImagePromise(memeType);
+
+  while (!acceptedType.includes(fileType(url))) {
+    url = await getImagePromise(memeType);
+  }
+  return url;
+};
+
+export default memeUrl;
