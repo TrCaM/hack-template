7d2cd07ae8099077d6a8f2235034e4ce02b5f17a
TrCaM <cmthscb@gmail.com>
2020-01-10 19:47:32 -0500

    Add favorites

--- src/screens/HomeScreen/FavoriteScreen.js
+++ src/screens/HomeScreen/FavoriteScreen.js
@@ -2,15 +2,12 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import MemeList from '../../../src/components/MemeList';
 import { Container } from 'native-base';
+import FavoriteList from '../../components/FavoriteList';
 
 const FavoriteScreen = props => {
   return (
     <Container>    
-      <View style={{padding: 100}}>
-        <Text>Favorite</Text>
-      </View>
-      <MemeList>
-      </MemeList>
+      <FavoriteList />
     </Container>
 
   );
