db1a751a5d9288548f394d740281bcac16335450
quanghienle <quanghien.a1@gmail.com>
2020-01-10 19:49:54 -0500

    fixed ios

--- src/screens/HomeScreen/FavoriteScreen.js
+++ src/screens/HomeScreen/FavoriteScreen.js
@@ -5,10 +5,7 @@ import { Container } from 'native-base';
 
 const FavoriteScreen = props => {
   return (
-    <Container>    
-      <View style={{padding: 100}}>
-        <Text>Favorite</Text>
-      </View>
+    <Container> 
       <MemeList>
       </MemeList>
     </Container>
